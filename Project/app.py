a6177b2ba5dfebfaed56c21cf7415689147f7729
ANILKUMARREDDYMURRA <vnandakishore882@gmail.com>
2026-04-05 15:46:26 +0530

    Fixed the UI and fixed scalar bug and added classification and feature importance

--- Project/app.py
+++ Project/app.py
@@ -1,49 +1,76 @@
-from flask import Flask, render_template, request
-import pandas as pd
+from flask import Flask, render_template, request, redirect, url_for
 import numpy as np
-import sklearn
 import os
 import pickle
-import warnings
-
+from urllib.parse import urlencode
+ 
 app = Flask(__name__)
-
-loaded_model = pickle.load(open("Project\model.pkl", 'rb'))
-
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+loaded_model = pickle.load(open(os.path.join(BASE_DIR, "model.pkl"), 'rb'))
+loaded_scaler = pickle.load(open(os.path.join(BASE_DIR, "scaler.pkl"), 'rb'))
 
 @app.route('/')
 def home():
-    return render_template('index.html')
-
+    nitrogen    = request.args.get('nitrogen', '')
+    phosphorus  = request.args.get('phosphorus', '')
+    potassium   = request.args.get('potassium', '')
+    temperature = request.args.get('temperature', '')
+    humidity    = request.args.get('humidity', '')
+    ph          = request.args.get('ph', '')
+    rainfall    = request.args.get('rainfall', '')
+    prediction  = request.args.get('prediction', '')
+ 
+    return render_template('index.html',
+        nitrogen    = nitrogen,
+        phosphorus  = phosphorus,
+        potassium   = potassium,
+        temperature = temperature,
+        humidity    = humidity,
+        ph          = ph,
+        rainfall    = rainfall,
+        prediction  = prediction,
+    )
 
 @app.route('/predict', methods=['POST'])
 def predict():
-    N = int(request.form['Nitrogen'])
-    P = int(request.form['Phosporus'])
-    K = int(request.form['Potassium'])
-    temp = float(request.form['Temperature'])
+    N        = int(request.form['Nitrogen'])
+    P        = int(request.form['Phosporus'])
+    K        = int(request.form['Potassium'])
+    temp     = float(request.form['Temperature'])
     humidity = float(request.form['Humidity'])
-    ph = float(request.form['pH'])
+    ph       = float(request.form['pH'])
     rainfall = float(request.form['Rainfall'])
-
+ 
     feature_list = [N, P, K, temp, humidity, ph, rainfall]
-    single_pred = np.array(feature_list).reshape(1, -1)
-
-    prediction = loaded_model.predict(single_pred)
-
-    crop_dict = {1: "Rice", 2: "Maize", 3: "Jute", 4: "Cotton", 5: "Coconut", 6: "Papaya", 7: "Orange",
-                 8: "Apple", 9: "Muskmelon", 10: "Watermelon", 11: "Grapes", 12: "Mango", 13: "Banana",
-                 14: "Pomegranate", 15: "Lentil", 16: "Blackgram", 17: "Mungbean", 18: "Mothbeans",
-                 19: "Pigeonpeas", 20: "Kidneybeans", 21: "Chickpea", 22: "Coffee"}
-
+    single_pred  = np.array(feature_list).reshape(1, -1)
+    input_scaled = loaded_scaler.transform([[N, P, K, temp, humidity, ph, rainfall]])
+    prediction   = loaded_model.predict(input_scaled)
+ 
+    crop_dict = {
+        1: "Rice", 2: "Maize", 3: "Jute", 4: "Cotton", 5: "Coconut",
+        6: "Papaya", 7: "Orange", 8: "Apple", 9: "Muskmelon", 10: "Watermelon",
+        11: "Grapes", 12: "Mango", 13: "Banana", 14: "Pomegranate", 15: "Lentil",
+        16: "Blackgram", 17: "Mungbean", 18: "Mothbeans", 19: "Pigeonpeas",
+        20: "Kidneybeans", 21: "Chickpea", 22: "Coffee"
+    }
+ 
     if prediction[0] in crop_dict:
-        crop = crop_dict[prediction[0]]
+        crop   = crop_dict[prediction[0]]
         result = "{} is the best crop to be cultivated right there".format(crop)
     else:
-        result = "Sorry, we could not determine the best crop to be cultivated with the provided data."
-
-    return render_template('index.html', prediction=result)
-
+        result = "Sorry, we could not determine the best crop with the provided data."
+ 
+    params = urlencode({
+        'nitrogen':    N,
+        'phosphorus':  P,
+        'potassium':   K,
+        'temperature': temp,
+        'humidity':    humidity,
+        'ph':          ph,
+        'rainfall':    rainfall,
+        'prediction':  result,
+    })
+    return redirect(url_for('home') + '?' + params)
 
 if __name__ == '__main__':
     app.run(debug=True)
